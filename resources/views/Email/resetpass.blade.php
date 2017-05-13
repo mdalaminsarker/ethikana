847f952b24b7ef88c4187a65cf6bc3b3f0c8c400
Adnan Shahriar <ssadnan1005@gmail.com>
2017-05-14 01:39:29 +0600

    Add files via upload

---
+++ resources/views/Email/resetpass.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<p>Someone requested a New Password for your BariKoi Account.</p><p>Your Temporary Password:<h3>{{$tempPass}}</h3></p>
+	Please update your password after logging into Your BariKoi Account.</p>
+</body>
+</html>
+
